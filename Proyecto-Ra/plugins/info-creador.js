e5445001220241f5a36b7338737ac6fd3723164e
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-06-27 00:23:48 -0500

    Update info-creador.js

--- Proyecto-Ra/plugins/info-creador.js
+++ Proyecto-Ra/plugins/info-creador.js
@@ -4,5 +4,26 @@ const handler = async (m, {conn}) => {
 handler.command = /^(Creador|Vendedores)$/i;
 export default handler;
 global.Creador = `
+Creador De : 𝙋𝙍𝙊𝙔𝙀𝘾𝙏𝙊 𝙍.𝘼 🕷️
 
+👤Ig : https://www.instagram.com/proyecto_r.a_
+*🇪🇨 RICARDO CEO 
+Contact :* https://wa.me/593979133620
+
+
+👤 Ig : https://www.instagram.com/proyecto_r.a_
+*🇩🇴 ANTHONY CEO
+ Contact :* https://wa.me/50764206082
+
+👤 Ig : https://www.instagram.com/proyecto_r.a_
+*🇪🇨 ELI CO-CEO
+Contact :* https://wa.me/593983025939
+
+👤 Ig : https://www.instagram.com/proyecto_r.a_
+*🇲🇽 ALE CO-CEO
+Contact :* https://wa.me/+529991091929
+
+👤 Ig : https://www.instagram.com/proyecto_r.a_
+*🇻🇪 SEMI CO-CEO
+ Contact :* https://wa.me/+584262668729
 `;
